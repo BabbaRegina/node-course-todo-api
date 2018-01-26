72ea7ca558299f24f64e2cf41af095a89370c63c
Irene Nicolini <inicolini@iks.local>
2018-01-26 10:42:12 +0100

    Setup separate env

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-const dbURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp';
+const dbURI = process.env.MONGODB_URI;
 mongoose.connect(dbURI);
 
 
--- server/server.js
+++ server/server.js
@@ -1,3 +1,7 @@
+
+
+require('./config/config');
+
 const _  = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -9,7 +13,7 @@ var { User } = require('./models/user');
 
 var app = express();
 
-const port = process.env.PORT || 3000; // porta per Heroku
+const port = process.env.PORT; 
 
 app.use(bodyParser.json()); // middleware
 
